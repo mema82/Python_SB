a4c5e2c2521e2a45d725d517c5acba8d89bcb1f4
Melissa Malpica <Malpicadz@gmail.com>
2015-02-09 12:53:20 -0500

    Practice with Python. creating variables and using operatord to print out results.

---
+++ index.py
@@ -0,0 +1,15 @@
+#Write a program to prompt user for the hours and rate per hour to computr gross pay.
+
+#Enter Hours 35
+
+#Enter Rate 2.75
+
+#Pay 96.25
+
+hours = 35
+
+rate = 2.75
+
+pay = rate * hours 
+
+print pay//result was correct with 96.25
